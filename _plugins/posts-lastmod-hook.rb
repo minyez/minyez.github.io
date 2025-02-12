ffe047563a6c37367b89e88833268b0ecc24a2b1
Minye Zhang <minyez.physchem@gmail.com>
2025-02-12 11:21:08 +0800

    refactor(plugin): separate set_lastmod_date

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -3,12 +3,14 @@
 # Check for changed posts
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  set_lastmod_date(post)
+end
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+def set_lastmod_date(doc)
+  commit_num = `git rev-list --count HEAD "#{ doc.path }"`
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ doc.path }"`
+    doc.data['last_modified_at'] = lastmod_date
   end
-
 end
